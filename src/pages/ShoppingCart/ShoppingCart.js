3fde319e7a5d76f90ee09f25747b48f8f8693711
luis1392 <luis.enri1392@gmail.com>
2020-09-23 13:28:06 -0500

    create shopping cart page

---
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+import ContainerShoppingCart from "../../components/ContainerShoppingCart";
+
+const ShoppingCart = () => {
+  return (
+    <Layout>
+      <ContainerShoppingCart />
+    </Layout>
+  );
+};
+
+export default ShoppingCart;
